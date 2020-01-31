d1ffc05f773d1b6f6138fa6e501d61ae2fdafa8f
GP-ZY <2804543032@qq.com>
2020-01-31 10:51:07 +0800

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <clip-image></clip-image>
+      <div class="resultBox">
+          <img src="" alt="">
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ClipImage from './components/ClipImage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ClipImage
   }
 }
 </script>
